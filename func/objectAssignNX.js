e8182688f68cc56750df03dd455c7a18712f090a
hui.wang <hui.wang@centurygame.com>
2023-04-18 11:54:56 +0800

    refactor: use cell as slot name

--- func/objectAssignNX.js
+++ func/objectAssignNX.js
@@ -1,12 +1,14 @@
 const objectEach = require("./objectEach");
 const hasOwnProp = require("./hasOwnProp");
 const objectAssignNX = function (target, options) {
+    target = target || {}
     objectEach(options, function (val, key) {
         if (hasOwnProp(target, key)) {
             return
         }
         target[key] = val
     })
+    return target
 }
 
 module.exports = objectAssignNX
