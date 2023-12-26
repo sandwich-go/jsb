31f7bd00f21504ba1f2402c87314bed1023fd8ac
hui.wang <hui.wang@centurygame.com>
2023-12-26 14:24:24 +0800

    fix: longestHTMLWidth

--- func/htmlWidth.js
+++ func/htmlWidth.js
@@ -1,12 +1,18 @@
-function htmlWidth(htmlStr = '') {
-    const div = document.createElement('div');
-    div.style.display = 'inline-block';
-    div.style.visibility = 'hidden';
-    div.innerHTML = htmlStr;
-    document.body.appendChild(div);
-    const width = div.clientWidth;
-    document.body.removeChild(div);
-    return width;
-}
+async function htmlWidth(htmlStr) {
+    return new Promise((resolve) => {
+        const div = document.createElement('div');
+        div.style.display = 'inline-block';
+        div.style.visibility = 'hidden';
+        div.innerHTML = htmlStr;
 
+        const calculateWidth = () => {
+            document.body.appendChild(div);
+            const width = div.clientWidth;
+            document.body.removeChild(div);
+            resolve(width);
+        };
+
+        requestAnimationFrame(calculateWidth);
+    });
+}
 module.exports = htmlWidth
--- func/longestHTMLWidth.js
+++ func/longestHTMLWidth.js
@@ -2,20 +2,20 @@ const htmlWidth = require("./htmlWidth");
 const isEmpty = require("./isEmpty");
 const cc = require("./cc");
 
-function longestHTMLWidth(nameList) {
+async function longestHTMLWidth(nameList) {
     const baseWithWhenZero = cc.textBaseWidth || 30
-    if(isEmpty(nameList) || nameList.length === 0){
+    if (isEmpty(nameList) || nameList.length === 0) {
         return baseWithWhenZero
     }
-    let longest = htmlWidth(nameList.sort(
+    let longest = await htmlWidth(nameList.sort(
         function (a, b) {
             return b.length - a.length;
         }
     )[0]);
-    if (!longest){
+    if (!longest) {
         longest = 0
     }
-    return  longest + baseWithWhenZero
+    return longest + baseWithWhenZero
 }
 
 module.exports = longestHTMLWidth
