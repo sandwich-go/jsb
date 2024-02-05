b2b1e3d7448218ae85a6767e0319c7d478fc4814
hui.wang <hui.wang@centurygame.com>
2024-02-05 13:38:20 +0800

    fix: tree 2 array

---
+++ func/treeToArray.js
@@ -0,0 +1,36 @@
+const cc = require("./cc");
+const each = require('./each');
+const assign = require('./assign');
+
+function unTreeList (result, array, opts) {
+    const optChildren = opts.children;
+    const optData = opts.data;
+    const optClear = opts.clear;
+    each(array, function (item) {
+        const children = item[optChildren];
+        if (optData) {
+            item = item[optData]
+        }
+        result.push(item)
+        if (children && children.length) {
+            unTreeList(result, children, opts)
+        }
+        if (optClear) {
+            delete item[optChildren]
+        }
+    })
+    return result
+}
+
+/**
+ * 将一个树结构转成数组列表
+ *
+ * @param {Array} array 数组
+ * @param {Object} options { children: 'children', data: 'data', clear: false }
+ * @return {Array}
+ */
+function toTreeArray (array, options) {
+    return unTreeList([], array, assign({}, cc.treeOptions, options))
+}
+
+module.exports = toTreeArray
