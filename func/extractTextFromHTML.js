e5a690484507b328f452af3eaeed16dba38aebdb
hui.wang <hui.wang@centurygame.com>
2024-02-07 00:24:27 +0800

    fix: aim up

--- func/extractTextFromHTML.js
+++ func/extractTextFromHTML.js
@@ -9,4 +9,4 @@ function  extractTextFromHTML(html, param = {}) {
     return new DOMParser().parseFromString(html, 'text/html').body.innerText;
 }
 
-module.eachTree = extractTextFromHTML
+module.exports = extractTextFromHTML
