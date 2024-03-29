448500396d794d344a2a90e01bb98654e3864ab3
hui.wang <hui.wang@centurygame.com>
2024-03-29 19:10:58 +0800

    timestamp

--- func/cc.js
+++ func/cc.js
@@ -34,6 +34,20 @@ const defaultCC = {
         key: 'id',
         children: 'children'
     },
+    colorPredefined:[
+        '#ff4500',
+        '#ff8c00',
+        '#ffd700',
+        '#90ee90',
+        '#00ced1',
+        '#1e90ff',
+        '#c71585',
+        'rgba(255, 69, 0, 0.68)',
+        'rgb(255, 120, 0)',
+        'hsl(181, 100%, 37%)',
+        'hsla(209, 100%, 56%, 0.73)',
+        '#c7158577'
+    ]
 }
 
 defaultCC.confirmDelete = defaultCC.confirm
