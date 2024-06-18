aa30b47fffb8ff9e53c191edf8360a40742cf46d
hui.wang <hui.wang@centurygame.com>
2024-06-18 18:39:22 +0800

    feat: walk

--- func/walk.js
+++ func/walk.js
@@ -4,6 +4,7 @@ const objectEach = require('./objectEach');
 
 /**
  * 迭代器，与each不同的是，如果迭代器返回的数值不是true，则中断迭代并返回该数值
+ * 如果obj为空则返回true
  *
  * @param {Object} obj 对象/数组
  * @param iterate
@@ -14,7 +15,7 @@ function walk (obj, iterate, thisArg= undefined) {
     if (obj) {
         return (isArray(obj) ? arrayEach : objectEach)(obj, iterate,true, thisArg)
     }
-    return obj
+    return true
 }
 
 module.exports = walk
