683ed1b26109cb2dbd0d137c52552e6c48ad1506
hui.wang <hui.wang@centurygame.com>
2024-06-19 17:05:49 +0800

    fix: list.forEach

--- func/arrayEach.js
+++ func/arrayEach.js
@@ -1,11 +1,25 @@
 const isBoolean = require("./isBoolean");
+const {re} = require("@babel/core/lib/vendor/import-meta-resolve");
 
 function arrayEach(list, iterate,stopWhenNotTrue=false,context=undefined) {
     if (list) {
-        for (let index = 0, len = list.length; index < len; index++) {
-            const result = iterate.call(context, list[index], index, list);
-            if (stopWhenNotTrue && result !== true) {
-                return result;
+        if (list.forEach) {
+            let shouldContinue = true
+            let result= undefined
+            list.forEach((item,index)=>{
+                if(shouldContinue){
+                    result = iterate.call(context, list[index], index, list);
+                    if (stopWhenNotTrue && result !== true) {
+                        shouldContinue = false
+                    }
+                }
+            }, context)
+        }else{
+            for (let index = 0, len = list.length; index < len; index++) {
+                const result = iterate.call(context, list[index], index, list);
+                if (stopWhenNotTrue && result !== true) {
+                    return result;
+                }
             }
         }
     }
